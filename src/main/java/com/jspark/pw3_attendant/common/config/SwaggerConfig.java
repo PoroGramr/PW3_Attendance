f10283c70ed72f5e8180618eca3e7d4379196abe
HoyiTT <pjs9177@naver.com>
2025-06-20 14:47:00 +0900

    [feat] local, dev swagger 설정 변경

--- src/main/java/com/jspark/pw3_attendant/common/config/SwaggerConfig.java
+++ src/main/java/com/jspark/pw3_attendant/common/config/SwaggerConfig.java
@@ -1,11 +1,19 @@
 package com.jspark.pw3_attendant.common.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+    servers = {
+        @Server(url = "http://localhost:8080", description = "Local Dev Server"),
+        @Server(url = "https://pw3api.porogramr.site", description = "Production Server")
+    }
+)
 public class SwaggerConfig {
 
     @Bean
