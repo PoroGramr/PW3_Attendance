e9703ddb4acbd9fcc126f899e9622c602d275e42
HoyiTT <pjs9177@naver.com>
2025-12-13 23:34:46 +0900

    [refactor] student request 직렬화 오류 해결

--- src/main/java/com/jspark/pw3_attendant/service/Student/dto/StudentRequest.java
+++ src/main/java/com/jspark/pw3_attendant/service/Student/dto/StudentRequest.java
@@ -1,5 +1,10 @@
 package com.jspark.pw3_attendant.service.Student.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.jspark.pw3_attendant.domain.Student.Student.Sex;
 import java.time.LocalDate;
 import lombok.Getter;
@@ -9,6 +14,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class StudentRequest {
     private String name;
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate birth;
     private Sex sex;
     private String phone;
