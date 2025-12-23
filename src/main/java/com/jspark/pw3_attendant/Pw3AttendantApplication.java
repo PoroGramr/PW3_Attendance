13e957902e2a4b5a826380a6c502e317ff68a085
HoyiTT <pjs9177@naver.com>
2025-12-23 13:46:22 +0900

    [feat] 출석 리포트 스케줄러 추가

--- src/main/java/com/jspark/pw3_attendant/Pw3AttendantApplication.java
+++ src/main/java/com/jspark/pw3_attendant/Pw3AttendantApplication.java
@@ -2,8 +2,10 @@ package com.jspark.pw3_attendant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Pw3AttendantApplication {
 
 	public static void main(String[] args) {
