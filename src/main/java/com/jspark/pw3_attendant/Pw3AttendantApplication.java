03025a29aeec0af243f76dde77f5b378ef036213
HoyiTT <pjs9177@naver.com>
2025-12-29 15:06:37 +0900

    Revert "[feat] 출석 리포트 스케줄러 추가"
    This reverts commit 13e957902e2a4b5a826380a6c502e317ff68a085.

--- src/main/java/com/jspark/pw3_attendant/Pw3AttendantApplication.java
+++ src/main/java/com/jspark/pw3_attendant/Pw3AttendantApplication.java
@@ -2,10 +2,8 @@ package com.jspark.pw3_attendant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class Pw3AttendantApplication {
 
 	public static void main(String[] args) {
