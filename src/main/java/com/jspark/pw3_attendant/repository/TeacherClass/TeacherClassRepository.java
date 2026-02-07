6d66136a528d1e81de0369b401e67aa07ed9f722
HoyiTT <pjs9177@naver.com>
2026-02-07 17:55:21 +0900

    [fix] 출석여부 조회시 N+1 해경

--- src/main/java/com/jspark/pw3_attendant/repository/TeacherClass/TeacherClassRepository.java
+++ src/main/java/com/jspark/pw3_attendant/repository/TeacherClass/TeacherClassRepository.java
@@ -1,22 +1,24 @@
 package com.jspark.pw3_attendant.repository.TeacherClass;
 
-
 import com.jspark.pw3_attendant.domain.ClassRoom.ClassRoom;
 import com.jspark.pw3_attendant.domain.Teacher.Teacher;
 import com.jspark.pw3_attendant.domain.TeacherClass.TeacherClass;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TeacherClassRepository extends JpaRepository<TeacherClass, Long> {
-    Optional<TeacherClass> findByClassRoomIdAndSchoolYear(Long classRoomId,Integer schoolYear);
+    Optional<TeacherClass> findByClassRoomIdAndSchoolYear(Long classRoomId, Integer schoolYear);
+
     List<TeacherClass> findAllByClassRoomIdAndSchoolYear(Long classRoomId, int schoolYear);
 
     List<TeacherClass> findAllBySchoolYear(Integer schoolYear);
 
     List<TeacherClass> findAllByTeacher(Teacher teacher);
 
+    @EntityGraph(attributePaths = { "teacher", "classRoom" })
     List<TeacherClass> findAllBySchoolYearAndClassRoomIn(Integer schoolYear, List<ClassRoom> classRooms);
 }
